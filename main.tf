5869e86b455a2872b5224ced1b41c68b0e9ce374
wmdeneth <dlm2002pc@gmail.com>
2026-01-31 09:09:00 +0530

    Test webhook auto-trigger

--- main.tf
+++ main.tf
@@ -59,6 +59,10 @@ resource "aws_instance" "project_server" {
               sudo systemctl start docker
               sudo systemctl enable docker
               sudo usermod -aG docker ubuntu
+              
+              # Install Docker Compose
+              sudo curl -L "https://github.com/docker/compose/releases/download/v2.24.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+              sudo chmod +x /usr/local/bin/docker-compose
               EOF
 
   tags = {
